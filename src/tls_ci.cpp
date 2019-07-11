9938f55c6fe760235d2158d152ee6cb3f867bd7e
Victor Telnov <v-telnov@yandex.ru>
2019-07-11 07:05:35 +0300

    Update tls_ci.cpp

--- src/tls_ci.cpp
+++ src/tls_ci.cpp
@@ -4,21 +4,26 @@
 std::pair<double,double> borders_ci(
         uint ar_events[], uint sz, double confidence_interval) {
 
+    // ar_events - точки графика с накопленной статистикой
+    // sz - количество точек графика
+       
     uint cnt = 0;
-    // всего точек в массиве данных
+    // всего данных в массиве данных
     for (uint i = 0; i < sz; ++i) cnt += ar_events[i];
 
-    // количество лишних точек слева и справа
+    // количество лишних данных слева и справа
     double proc_cnt = (1.0 - confidence_interval) * cnt / 2.0;
 
     double lo = 0;
 
     uint su = 0;
-    // отсчитываю лишние точки слева
+    // отсчитываю данные слева
     for (uint i = 0; i < sz; ++i) {
         uint c_in_stp = ar_events[i];
 
         if (su + c_in_stp >= proc_cnt) {
+            // граница определяется пропорцией по точке графика,
+            //   размерность одной точки графика это 1.0/sz
             lo = (DD(i) + (proc_cnt - DD(su)) / DD(c_in_stp)) / DD(sz);
             break;
         }
@@ -28,7 +33,7 @@ std::pair<double,double> borders_ci(
 
     double hi = 0;
     su = 0;
-    // отсчитываю лишние точки справа
+    // отсчитываю данные справа
     for (uint i = sz; i > 0; --i) {
         uint c_in_stp = ar_events[i-1];
 
